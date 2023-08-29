618dbbabf2c6f68c1dd5b87334d982a951ebeb1e
이경민(Kyung Min Lee) <kminsmin@gmail.com>
2023-08-29 15:49:46 +0900

    Character 클래스 생성

--- Model/Character.cs
+++ Model/Character.cs
@@ -9,6 +9,24 @@ namespace TextRPGing.Model
 {
     internal class Character : CharacterController
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Level { get; set; }
+        public int HP { get; set; }
+        public int MaxHP { get; set; }
+        public int ATK { get; set; }
+        public int DEF { get; set; }
+        public float CRT { get; set; }
+        public float AVD { get; set; }
+        enum characterClass
+        {
+            Warrior,
+            Thief,
+            Archer,
+            Magician
+        }
+        Inventory Inven { get; set; }
+        Equipment Equip { get; set; }
 
     }
 }
--- Model/Equipment.cs
+++ Model/Equipment.cs
@@ -4,10 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TextRPGing.Controller
+namespace TextRPGing.Model
 {
-    internal class CharacterController
+    internal class Equipment
     {
-
     }
 }
