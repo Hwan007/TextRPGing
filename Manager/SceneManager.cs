893b6caf9f8692182ed8698bd16306f082a819e7
Hwan007 <ghksdlco@gmail.com>
2023-08-29 18:56:18 +0900

    ChangeScene, GetEnableScene 완성

--- Manager/SceneManager.cs
+++ Manager/SceneManager.cs
@@ -8,19 +8,73 @@ namespace TextRPGing.Manager
 {
     public class SceneManager
     {
+        public static SceneManager instance = null;
+        private int[,] mRoadMap;
+        private Define.GameEnum.eSceneType mCurrentScene;
+
+        public SceneManager()
+        {
+            mRoadMap = new int[(int)Define.GameEnum.eSceneType.End+1, (int)Define.GameEnum.eSceneType.End+1];
+            MapSetting();
+            if (instance == null)
+                instance = this;
+            mCurrentScene = Define.GameEnum.eSceneType.Main;
+        }
         public void ActByInput(int input)
         {
 
         }
-
         public void ChangeScene(Define.GameEnum.eSceneType sceneType)
         {
-
+            if (mRoadMap[(int)mCurrentScene, (int)sceneType] == 1)
+            {
+                mCurrentScene = sceneType;
+            }
+            else
+                throw new Exception($"{mCurrentScene} -> {sceneType} 허락되지 않은 Scene 이동입니다.");
         }
-
         public Define.GameEnum.eSceneType[] GetEnableScene()
         {
-            throw new NotImplementedException();
+            List<Define.GameEnum.eSceneType> ret = new List<Define.GameEnum.eSceneType>();
+            for (int i = 0; i < mRoadMap.GetLength((int)mCurrentScene);)
+            {
+                if (mRoadMap[(int)mCurrentScene, i] == 1)
+                    ret.Add((Define.GameEnum.eSceneType)i);
+            }
+            return ret.ToArray();
+        }
+        private void MapSetting()
+        {
+            for (int i = 0; i <= (int)Define.GameEnum.eSceneType.SaveLoad; ++i)
+            {
+                mRoadMap[i, i] = 0;
+                switch ((Define.GameEnum.eSceneType)i)
+                {
+                    case Define.GameEnum.eSceneType.Status:
+                        mRoadMap[i, (int)Define.GameEnum.eSceneType.Main] = 1;
+                        break;
+                    case Define.GameEnum.eSceneType.Battle:
+                        mRoadMap[i, (int)Define.GameEnum.eSceneType.Main] = 1;
+                        break;
+                    case Define.GameEnum.eSceneType.Recovery:
+                        mRoadMap[i, (int)Define.GameEnum.eSceneType.Main] = 1;
+                        break;
+                    case Define.GameEnum.eSceneType.SaveLoad:
+                        mRoadMap[i, (int)Define.GameEnum.eSceneType.Main] = 1;
+                        break;
+                    case Define.GameEnum.eSceneType.Main:
+                        mRoadMap[i, (int)Define.GameEnum.eSceneType.Battle] = 1;
+                        mRoadMap[i, (int)Define.GameEnum.eSceneType.Recovery] = 1;
+                        mRoadMap[i, (int)Define.GameEnum.eSceneType.SaveLoad] = 1;
+                        mRoadMap[i, (int)Define.GameEnum.eSceneType.Main] = 1;
+                        break;
+                    case Define.GameEnum.eSceneType.End:
+                        mRoadMap[i, (int)Define.GameEnum.eSceneType.Main] = 1;
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
     }
 }
