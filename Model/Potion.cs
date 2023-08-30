43abf5b5de0c89aed829fb3e2fd6adbcccadb4eb
yarogono <70641418+yarogono@users.noreply.github.com>
2023-08-30 16:41:12 +0900

    feat/Potion Weapon Armor 추가

---
+++ Model/Potion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPGing.Model
+{
+    public class Potion : Item
+    {
+        public int Heal { get; set; }
+    }
+}
