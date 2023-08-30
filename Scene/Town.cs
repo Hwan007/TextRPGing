4db85e168e8e35f76710303a8808c227ad26dddf
Hwan007 <ghksdlco@gmail.com>
2023-08-30 15:46:59 +0900

    Town 클래서 생성

---
+++ Scene/Town.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPGing.Scene
+{
+    public class Town : Define.Interface.IScene
+    {
+        public bool ActByInput(int input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MainLoop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
