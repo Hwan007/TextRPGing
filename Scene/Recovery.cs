0e4dd2aed85033838f8397345c1b74fbeaea18ff
이경민(Kyung Min Lee) <kminsmin@gmail.com>
2023-08-30 16:24:18 +0900

    Character Inven, Equip => public

--- Scene/Recovery.cs
+++ Scene/Recovery.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using TextRPGing.Define.Interface;
 using TextRPGing.Manager;
+using TextRPGing.Model;
 using TextRPGing.Utils;
 
 namespace TextRPGing.Scene
@@ -12,41 +13,74 @@ namespace TextRPGing.Scene
     public class Recovery : IScene
     {
         Define.GameEnum.eSceneType type = Define.GameEnum.eSceneType.Recovery;
-        UIManager _uiManager = new UIManager();
+        private int tempHP;
+        public enum HealingState
+        {
+            noHeal,
+            isHeal,
+            isFull
+        }
         public void MainLoop()
         {
+            HealingState healingState = HealingState.noHeal;
             DisplayRecoveryScene();        
+            switch(healingState)
+            {
+                case HealingState.noHeal:
+                    DisplayRecoveryScene();
+                    break;
+                case HealingState.isFull:
+                    break;
 
+            }
 
         }
         public bool ActByInput(int input)
         {
             if (input == 1)
             {
-
+                tempHP = Character.Player.HP;
+                Character.Player.TakeHeal(30);
+                int healAmount = Character.Player.HP - tempHP;
+                if (healAmount > 0)
+                {
+                    
+                }
+                return true;
+            }
+            else if (input == 0)
+            {
+                GameManager.SceneManager.ChangeScene(Define.GameEnum.eSceneType.Town); return true;
             }
             return false;
         }
 
         private void DisplayRecoveryScene()
         {
-            _uiManager.ConsoleClear();
-            string message = "회복\n아이템을 사용해 체력을 30 회복할 수 있습니다.\n소지개수 : {개수}\n\n체력 : \n\n\n\n\n1. 회복\n0. 나가기";
+            GameManager.UIManager.ConsoleClear();
+            string message = "회복\n아이템을 사용해 체력을 30 회복할 수 있습니다.\r\n";
 
             MessageToUIManager(message);
+        }
+
+        private void DisplayChoice()
+        {
+            string message = $"소지개수 : [개수]\n\n체력: {Character.Player.HP}/{Character.Player.MaxHP}\n\n\n\n\n" +
+                               " ";
+                
         }
 
 
         private void MessageToUIManager(string message)
         {
             MessageToUI messageToUi = new MessageToUI(
-                Define.GameEnum.eSceneType.Main,
+                Define.GameEnum.eSceneType.Town,
                 new string[] { message }
             );
 
-            _uiManager.PutToOutQueue(messageToUi);
-            _uiManager.DisplayUpdate();
-            _uiManager.ClearMessageQueue();
+            GameManager.UIManager.PutToOutQueue(messageToUi);
+            GameManager.UIManager.DisplayUpdate();
+            GameManager.UIManager.ClearMessageQueue();
         }
 
     }
