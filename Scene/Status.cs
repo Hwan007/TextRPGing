284bc8918ae80e2b2d04c88c0e4e8faafce4b86d
Hwan007 <ghksdlco@gmail.com>
2023-08-30 17:18:37 +0900

    Status Scene 완성

--- Scene/Status.cs
+++ Scene/Status.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TextRPGing.Manager;
+using TextRPGing.Model;
+using TextRPGing.Utils;
 
 namespace TextRPGing.Scene
 {
@@ -11,17 +13,60 @@ namespace TextRPGing.Scene
     {
         public bool ActByInput(int input)
         {
-            return false;
+            var Routes = GameManager.SceneManager.GetEnableScene();
+            if (Routes.Length >= input)
+                return false;
+            else
+            {
+                SceneManager.instance.ChangeScene(Routes[input]);
+                return true;
+            }
         }
 
         public void MainLoop()
         {
+            List<string> sbs = new List<string>();
+            StringBuilder sb = new StringBuilder();
             // 타이틀 출력
-            GameManager.UIManager
+            sb.Clear();
+            sb.Append("상태보기\n");
+            sb.Append("캐릭터의 정보가 표시됩니다.\n");
+            sbs.Add(sb.ToString());
 
             // 캐릭터 정보 출력
-
+            sb.Clear();
+            sb.Append($"LV. {Character.Player.Level}\n");
+            sb.Append($"{Character.Player.Name}  ({Character.Player.Job})\n");
+            sb.Append($"공격력 : {Character.Player.ATK}\n");
+            sb.Append($"방어력 : {Character.Player.DEF}\n");
+            sb.Append($"체  력 : {Character.Player.HP}/{Character.Player.MaxHP}\n");
+            sb.Append($"골  드 : {Character.Player.Inven.Gold}\n");
+            sbs.Add(sb.ToString());
+            
             // 선택지 출력
+            sb.Clear();
+            var Routes = GameManager.SceneManager.GetEnableScene();
+            int i = 0;
+            foreach (var route in Routes)
+            {
+                switch (route)
+                {
+                    case Define.GameEnum.eSceneType.Town:
+                        sb.Append($"{i}. 나가기\n");
+                        break;
+                    case Define.GameEnum.eSceneType.Battle:
+                        sb.Append($"{i}. 전투 시작\n");
+                        break;
+                    default:
+                        sb.Append($"{i}. 오류입니다. 수정해주세요.\n");
+                        break;
+                }
+                ++i;
+            }
+            sbs.Add(sb.ToString());
+
+            MessageToUI message = new MessageToUI(Define.GameEnum.eSceneType.Status, sbs.ToArray());
+            GameManager.UIManager.PutToOutQueue(message);
         }
     }
 }
