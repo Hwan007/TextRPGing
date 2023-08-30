965282bf5d7915d735efba2ebce5b75812b440e1
yarogono <70641418+yarogono@users.noreply.github.com>
2023-08-30 16:06:33 +0900

    feat/SaveLoad 빈 클래스 생성

---
+++ Scene/SaveLoad.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextRPGing.Define.Interface;
+
+namespace TextRPGing.Scene
+{
+    public class SaveLoad : IScene
+    {
+        public bool ActByInput(int input)
+        {
+            // TODO
+            return false;
+        }
+
+        public void MainLoop()
+        {
+            // TODO
+            throw new NotImplementedException();
+        }
+    }
+}
