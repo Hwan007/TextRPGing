1d83f9797654d26d1174e37b7f19dd76c11a4e12
yarogono <70641418+yarogono@users.noreply.github.com>
2023-09-01 00:21:56 +0900

    feat/SaveLoad 초기 구현 완료

--- Scene/SaveLoad.cs
+++ Scene/SaveLoad.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using TextRPGing.Define;
 using TextRPGing.Define.Interface;
 using TextRPGing.Manager;
@@ -13,81 +10,93 @@ namespace TextRPGing.Scene
 {
     public class SaveLoad : IScene
     {
-        private Define.GameEnum.eSceneType type = Define.GameEnum.eSceneType.SaveLoad;
-
+        private Define.GameEnum.eSceneType _type = Define.GameEnum.eSceneType.SaveLoad;
+        private bool _isSaveLoadSuccess = false;
+        
         public bool ActByInput(int input, ref GameEnum.eSceneType scene)
         {
-            // 1 . 저장하기
-            // 2 . 불러오기
-            // 3. 다른 씬으로 이동
-
             switch (input)
             {
                 case 1:
                     IOStream.SavePlayerJsonFile();
+                    _isSaveLoadSuccess = true;
                     return true;
                 case 2:
                     Character.Player = IOStream.LoadPlayerJsonFile();
+                    _isSaveLoadSuccess = true;
                     return true;
                 case 3:
-                    string message = AppendRoutes().ToString();
-                    MessageToUIManager(message);
-                    break;
+                    GameManager.UIManager.ConsoleClear();
+                    DisplayRoute();
+                    var routes = GameManager.SceneManager.GetEnableScene(_type);
 
+                    int inputNum = int.Parse(Console.ReadLine());
+                    if (routes.Length < inputNum || inputNum < 0)
+                        return false;
+
+                    GameManager.SceneManager.ChangeScene(ref scene, routes[inputNum]);
+
+                    return true;
             }
+
             return false;
         }
 
         public void MainLoop()
         {
+            GameManager.UIManager.ConsoleClear();
             DisplaySaveLoadScene();
+        }
+
+
+        private void DisplayRoute()
+        {
+            GameManager.UIManager.ConsoleClear();
+            StringBuilder sb = new StringBuilder();
+            Define.GameEnum.eSceneType[] routes = GameManager.SceneManager.GetEnableScene(_type);
+            sb.AppendLine("다른 씬으로 이동");
+
+            for (int i = 0; i < routes.Length; i++) 
+            {
+                switch (routes[i])
+                {
+                    case Define.GameEnum.eSceneType.Town:
+                        sb.Append($"{i}. 마을\n");
+                        break;
+                    default:
+                        sb.Append("오류입니다. 수정하세요.\n");
+                        break;
+                }
+            }
+            sb.Append("\r\n" +
+                          "원하는 행동을 입력해주세요.\r\n" +
+                          ">> ");
+            MessageToUIManager(sb.ToString());
+
         }
 
         private void DisplaySaveLoadScene()
         {
-            GameManager.UIManager.ConsoleClear();
-            string message = "저장 및 불러오기\n" +
-                                   "\r\n" +
-                                   "1. 저장하기\r\n" +
-                                   "2. 불러오기\r\n" +
-                                   "3. 다른 씬으로 이동\r\n" +
-                                   "\r\n" +
-                                   "원하시는 행동을 입력하세요.\r\n" +
-                                   ">> ";
-
-            MessageToUIManager(message);
-        }
-
-
-        private StringBuilder AppendRoutes()
-        {
             StringBuilder sb = new StringBuilder();
-            Define.GameEnum.eSceneType[] routes = GameManager.SceneManager.GetEnableScene(type);
-            for (int i = 0; i < routes.Length; ++i)
+            if (_isSaveLoadSuccess)
             {
-                sb.Append($"{i}. ");
-                switch (routes[i])
-                {
-                    case Define.GameEnum.eSceneType.Status:
-                        sb.Append($"상태보기\n");
-                        break;
-                    case Define.GameEnum.eSceneType.Battle:
-                        sb.Append($"전투 시작\n");
-                        break;
-                    case Define.GameEnum.eSceneType.Recovery:
-                        sb.Append($"포션 사용\n");
-                        break;
-                    case Define.GameEnum.eSceneType.SaveLoad:
-                        break;
-                    default:
-                        sb.Append($"오류입니다. 수정해주세요.\n");
-                        break;
-                }
+                sb.Append("저장/불러오기가 완료되었습니다.\n");
+                _isSaveLoadSuccess = false;
             }
-            sb.Append("\n");
-            return sb;
-        }
+            else
+            {
+                sb.Append("저장 및 불러오기\n");
+            }
+            sb.Append( "\r\n" +
+                            "1. 저장하기\r\n" +
+                            "2. 불러오기\r\n" +
+                            "3. 다른 씬으로 이동\r\n" +
+                            "\r\n" +
+                            "원하시는 행동을 입력하세요.\r\n" +
+                            ">> ");
 
+            MessageToUIManager(sb.ToString());
+        }
 
 
         private void MessageToUIManager(string message)
