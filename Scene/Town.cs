e93464efcee9904237e4a49cfbe1d09f23f94adf
Hwan007 <ghksdlco@gmail.com>
2023-09-01 11:22:45 +0900

    버그 수정

--- Scene/Town.cs
+++ Scene/Town.cs
@@ -24,7 +24,6 @@ namespace TextRPGing.Scene
                 {
                     Character.Player = new Character(name, job.Value);
                     Character.Player.Equip = new Equipment();
-                    Character.Player.Equip.mCharacter = Character.Player;
                     SendToUIManager($"내일배움캠프에 온 것을 환영하오.");
                     GameManager.UIManager.DisplayUpdate();
                     GameManager.UIManager.ClearMessageQueue();
